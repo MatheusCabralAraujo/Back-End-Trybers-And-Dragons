94cb3201cce928c896835b043d00e3be0661dcc2
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2022-08-22 18:11:22 -0300

    feat: :construction: Added energy interface

---
+++ src/Races/index.ts
@@ -0,0 +1,13 @@
+import Race from './race';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
+import Halfling from './Halfling';
+import Orc from './Orc';
+
+export default Race;
+export {
+  Dwarf,
+  Elf,
+  Halfling,
+  Orc,
+};  
